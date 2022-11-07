22223794ef404089d9c2becb45107276ed3f991c
pawelb92 <pawel.brzezinski92@gmail.com>
2022-11-07 18:22:23 +0100

    AplikacjaBankowa Dodanie konta

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Account account1= new Account("Janusz","Januszewski","234144424424","adad","1234567890",10000,1234);
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("Witaj w naszym banku");
+        System.out.println("Użtkowniku wpisz login");
+        String login = sc.next();
+        System.out.println("Podaj kod pin");
+        int pin=sc.nextInt();
+
+    }
+}
